05a7de3c5559e38e3db248e6c1e0e90fdd23ce0e
Георги Дандиев <gd54376432@edu.mon.bg>
2023-01-19 18:04:25 +0200

    QOL

--- GoOutBurgas/Pages/LoginPage.xaml.cs
+++ GoOutBurgas/Pages/LoginPage.xaml.cs
@@ -18,41 +18,42 @@ public partial class LoginPage : ContentPage
 
     private async void OnButtonClicked(object sender, EventArgs e)
     {
-        var email = Email.Text;
-        var pass = Pass.Text;
-        if (string.IsNullOrEmpty(Email.Text) && string.IsNullOrEmpty(Pass.Text))
-        {
-            await DisplayAlert("Empty", "Empty no good", "OK");
-        }
-        else if (string.IsNullOrEmpty(Email.Text))
-        {
-            await DisplayAlert("Empty", "Email missing", "OK");
-        }
-        else if (string.IsNullOrEmpty(Pass.Text))
-        {
-            await DisplayAlert("Empty", "Password missing", "OK");
-        }
-        else
-        {
-            User loginTable = new User();
+        //var email = Email.Text;
+        //var pass = Pass.Text;
+        await Navigation.PushAsync(new NavigationPage(new MapPage()));
+        //if (string.IsNullOrEmpty(Email.Text) && string.IsNullOrEmpty(Pass.Text))
+        //{
+        //    await DisplayAlert("Empty", "Empty no good", "OK");
+        //}
+        //else if (string.IsNullOrEmpty(Email.Text))
+        //{
+        //    await DisplayAlert("Empty", "Email missing", "OK");
+        //}
+        //else if (string.IsNullOrEmpty(Pass.Text))
+        //{
+        //    await DisplayAlert("Empty", "Password missing", "OK");
+        //}
+        //else
+        //{
+        //    User loginTable = new User();
 
-            try
-            {
-                if (await notController.LoginCheck(Email.Text, Pass.Text) != null)
-                {
-                    await Navigation.PushAsync(new NavigationPage(new MapPage()));
-                }
-                else
-                {
-                    await DisplayAlert("Wrong", "Email or password ", "OK");
-                }
-            }
-            catch (Exception ex)
-            {
+        //    try
+        //    {
+        //        if (await notController.LoginCheck(Email.Text, Pass.Text) != null)
+        //        {
+        //            await Navigation.PushAsync(new NavigationPage(new MapPage()));
+        //        }
+        //        else
+        //        {
+        //            await DisplayAlert("Wrong", "Email or password ", "OK");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
 
-                await DisplayAlert("Error", "Something wrong happened: " + ex.Message, "PANIC");
-            }
-        }
+        //        await DisplayAlert("Error", "Something wrong happened: " + ex.Message, "PANIC");
+        //    }
+        //}
     }
     private async void TapGestureRecognizer_Tapped_For_SignUP(object sender, EventArgs e)
     {
--- GoOutBurgas/Pages/MapPage.xaml.cs
+++ GoOutBurgas/Pages/MapPage.xaml.cs
@@ -8,7 +8,5 @@ public partial class MapPage : ContentPage
 	{
 		InitializeComponent();
 
-        Map map = new Map();
-        Content = map;
     }
 }
