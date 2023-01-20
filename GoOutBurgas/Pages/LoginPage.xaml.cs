6a1c8ab55ff2e09be679082c860ced05666df403
Георги Дандиев <gd54376432@edu.mon.bg>
2023-01-20 16:58:09 +0200

    QOL

--- GoOutBurgas/Pages/LoginPage.xaml.cs
+++ GoOutBurgas/Pages/LoginPage.xaml.cs
@@ -1,6 +1,6 @@
 using GoOutBurgas.Data;
 using GoOutBurgas.Model;
-
+using GoOutBurgas.Themes;
 namespace GoOutBurgas.Pages;
 
 public partial class LoginPage : ContentPage
@@ -9,7 +9,14 @@ public partial class LoginPage : ContentPage
     public LoginPage()
     {
         InitializeComponent();
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
 
+        Application.Current.UserAppTheme = AppTheme.Light;
+        //if (mergedDictionaries != null)
+        //{
+        //    mergedDictionaries.Clear();
+        //    mergedDictionaries.Add(new LightTheme());
+        //}
     }
 
     private async void GoToRegister(object sender, EventArgs e)
@@ -19,42 +26,44 @@ public partial class LoginPage : ContentPage
 
     private async void OnButtonClicked(object sender, EventArgs e)
     {
-        //var email = Email.Text;
-        //var pass = Pass.Text;
-        await Navigation.PushAsync(new NavigationPage(new MapPage()));
-        //if (string.IsNullOrEmpty(Email.Text) && string.IsNullOrEmpty(Pass.Text))
-        //{
-        //    await DisplayAlert("Empty", "Empty no good", "OK");
-        //}
-        //else if (string.IsNullOrEmpty(Email.Text))
-        //{
-        //    await DisplayAlert("Empty", "Email missing", "OK");
-        //}
-        //else if (string.IsNullOrEmpty(Pass.Text))
-        //{
-        //    await DisplayAlert("Empty", "Password missing", "OK");
-        //}
-        //else
-        //{
-        //    User loginTable = new User();
+        //  await Navigation.PushAsync(new NavigationPage(new MapPage()));
 
-        //    try
-        //    {
-        //        if (await notController.LoginCheck(Email.Text, Pass.Text) != null)
-        //        {
-        //            await Navigation.PushAsync(new NavigationPage(new MapPage()));
-        //        }
-        //        else
-        //        {
-        //            await DisplayAlert("Wrong", "Email or password ", "OK");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
+        var email = Email.Text;
+        var pass = Pass.Text;
+     
+        if (string.IsNullOrEmpty(Email.Text) && string.IsNullOrEmpty(Pass.Text))
+        {
+            await DisplayAlert("Empty", "Empty textboxes not good no good", "OK");
+        }
+        else if (string.IsNullOrEmpty(Email.Text))
+        {
+            await DisplayAlert("Empty", "Email missing", "OK");
+        }
+        else if (string.IsNullOrEmpty(Pass.Text))
+        {
+            await DisplayAlert("Empty", "Password missing", "OK");
+        }
+        else
+        {
+            User loginTable = new User();
 
-        //        await DisplayAlert("Error", "Something wrong happened: " + ex.Message, "PANIC");
-        //    }
-        //}
+            try
+            {
+                if (await notController.LoginCheck(Email.Text, Pass.Text) != null)
+                {
+                    await Navigation.PushAsync(new NavigationPage(new MapPage()));
+                }
+                else
+                {
+                    await DisplayAlert("Wrong", "Email or password ", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error", "Something wrong happened: " + ex.Message, "PANIC");
+            }
+        }
     }
     private async void TapGestureRecognizer_Tapped_For_SignUP(object sender, EventArgs e)
     {
@@ -72,4 +81,53 @@ public partial class LoginPage : ContentPage
             Pass.IsPassword = true;
         }
     }
+
+    private async void OnPasswordCompleted(object sender, EventArgs e)
+    {
+        //  await Navigation.PushAsync(new NavigationPage(new MapPage()));
+
+        var email = Email.Text;
+        var pass = Pass.Text;
+
+        if (string.IsNullOrEmpty(Email.Text) && string.IsNullOrEmpty(Pass.Text))
+        {
+            await DisplayAlert("Empty", "Empty textboxes not good no good", "OK");
+        }
+        else if (string.IsNullOrEmpty(Email.Text))
+        {
+            await DisplayAlert("Empty", "Email missing", "OK");
+        }
+        else if (string.IsNullOrEmpty(Pass.Text))
+        {
+            await DisplayAlert("Empty", "Password missing", "OK");
+        }
+        else
+        {
+            User loginTable = new User();
+
+            try
+            {
+                if (await notController.LoginCheck(Email.Text, Pass.Text) != null)
+                {
+                    await Navigation.PushAsync(new NavigationPage(new MapPage()));
+                }
+                else
+                {
+                    await DisplayAlert("Wrong", "Email or password ", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error", "Something wrong happened: " + ex.Message, "PANIC");
+            }
+        }
+
+    }
+    private  void EmailCompleted(object sender, EventArgs e)
+    {
+       //string text = ((Entry)sender).Text;
+        string text = Pass.Text;
+    }
+
 }
